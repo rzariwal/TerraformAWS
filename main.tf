a612bd7b87b0c60308a6c5019b3e772b1eb7d382
rzariwal <rzariwal@gmail.com>
2022-12-05 11:47:37 +0800

    Modify files add subnets

--- main.tf
+++ main.tf
@@ -42,3 +42,42 @@ terraform {
     region = "ap-southeast-1"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  count      = "${length(var.public_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.public_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "public_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count      = "${length(var.private_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.private_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "private_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "database_subnet" {
+  count      = "${length(var.database_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.database_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "database_subnet_${count.index}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,23 @@ variable "vpc_cidr_block" {
   description = "Main VPC CIDR Block"
   default = "10.0.0.0/16"
 }
+variable "availability_zones" {
+  type = "list"
+  description = "AWS Region Availability Zones"
+  default = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
+}
+variable "public_subnet_cidr_block" {
+  type = "list"
+  description = "Public Subnet CIDR Block"
+  public_subnet_cidr_block = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
+variable "private_subnet_cidr_block" {
+  type = "list"
+  description = "Private Subnet CIDR Block"
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+variable "database_subnet_cidr_block" {
+  type = "list"
+  description = "Database Subnet CIDR Block"
+  default = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+}

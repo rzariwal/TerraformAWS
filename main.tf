8577215f1fc9f7d7cc0a6e366f044fb2c8a006b5
rzariwal <50871325+rzariwal@users.noreply.github.com>
2022-12-05 10:06:02 +0800

    Update main.tf
    Create VPC in Singapore

--- main.tf
+++ main.tf
@@ -1,63 +1,17 @@
-# Require TF version to be same as or greater than 0.12.13
-terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
-}
-
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+#-------------------------------
+# AWS Provider
+#-------------------------------
 provider "aws" {
-  region  = "us-east-1"
-  version = "~> 2.36.0"
+  region = "ap-southeast-1"
 }
-
-
-/* Commented out until after bootstrap
-
-# Call the seed_module to build our ADO seed info
-module "bootstrap" {
-  source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "kyler-github-actions-demo-terraform-tfstate"
-  dynamo_db_table_name        = "aws-locks"
-  iam_user_name               = "GitHubActionsIamUser"
-  ado_iam_role_name           = "GitHubActionsIamRole"
-  aws_iam_policy_permits_name = "GitHubActionsIamPolicyPermits"
-  aws_iam_policy_assume_name  = "GitHubActionsIamPolicyAssume"
-}
-
-# Build the VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.1.0.0/16"
-  instance_tenancy     = "default"
-
+ 
+#-------------------------------
+# VPC resource
+#-------------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+ 
   tags = {
-    Name      = "Vpc"
-    Terraform = "true"
+    Name = "my-terraform-aws-vpc"
   }
 }
-
-# Build route table 1
-resource "aws_route_table" "route_table1" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "RouteTable1"
-    Terraform = "true"
-  }
-}
-
-# Build route table 2
-resource "aws_route_table" "route_table2" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = {
-    Name = "RouteTable2"
-    Terraform = "true"
-  }
-}
-*/

4030b4d19823e5e6fa895f3cf2c18787bff72003
rzariwal <50871325+rzariwal@users.noreply.github.com>
2022-12-05 10:30:47 +0800

    Update main.tf
    Create VPC

--- main.tf
+++ main.tf
@@ -5,3 +5,39 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+#-------------------------------
+# VPC resource
+#-------------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+ 
+  tags = {
+    Name = "my-terraform-aws-vpc"
+  }
+}
+
+#-------------------------------
+# S3 Bucket Creation
+#-------------------------------
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-terraform-bucket-terraform-state"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+  
+  tags = {
+    Name = "my-terraform-bucket-terraform-state"
+  }
+}
+
+#-------------------------------
+# S3 Remote State
+#-------------------------------
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-bucket-terraform-state"
+    key    = "vpc.tfstate"
+    region = "ap-southeast-1"
+  }
+}

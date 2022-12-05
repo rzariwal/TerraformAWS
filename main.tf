1ccb85e1ce6b6db88c3a6c2533648892317388a2
rzariwal <50871325+rzariwal@users.noreply.github.com>
2022-12-05 10:28:48 +0800

    Update main.tf
    delete

--- main.tf
+++ main.tf
@@ -5,28 +5,3 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-#-------------------------------
-# S3 Bucket Creation
-#-------------------------------
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-terraform-bucket-terraform-state"
-  acl = "private"
-  versioning {
-    enabled = true
-  }
-  
-  tags = {
-    Name = "my-terraform-bucket-terraform-state"
-  }
-}
-
-#-------------------------------
-# S3 Remote State
-#-------------------------------
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-bucket-terraform-state"
-    key    = "vpc.tfstate"
-    region = "ap-southeast-1"
-  }
-}

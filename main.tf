2ef7d9b0f48d4daa70d88285506f28420869b5e8
rzariwal <50871325+rzariwal@users.noreply.github.com>
2022-12-05 10:23:56 +0800

    Update main.tf
    Delete VPC?

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
- 
-#-------------------------------
-# VPC resource
-#-------------------------------
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
- 
-  tags = {
-    Name = "my-terraform-aws-vpc"
-  }
-}
 
 #-------------------------------
 # S3 Bucket Creation

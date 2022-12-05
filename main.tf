e3ca6492c27c1b19165dda1887dcd976b3a85eb3
rzariwal <rzariwal@gmail.com>
2022-12-05 11:11:04 +0800

    Modify files delete VPC

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ provider "aws" {
 #-------------------------------
 # VPC resource
 #-------------------------------
-resource "aws_vpc" "main" {
+/*resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
  
   tags = {
     Name = "my-terraform-aws-vpc"
   }
-}
+}*/
 
 #-------------------------------
 # S3 Bucket Creation

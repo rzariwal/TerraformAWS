cc9e6f0f6567844b8bad26da55269acfd258f66c
rzariwal <rzariwal@gmail.com>
2022-12-05 13:57:16 +0800

    cleanup

--- main.tf
+++ main.tf
@@ -4,3 +4,210 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+
+#-------------------------------
+# VPC resource
+#-------------------------------
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-terraform-aws-vpc-${terraform.workspace}"
+    Environment = "${terraform.workspace}"
+  }
+}
+
+#-------------------------------
+# S3 Bucket Creation
+#-------------------------------
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-terraform-bucket-terraform-state"
+  acl = "private"
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "my-terraform-bucket-terraform-state"
+  }
+}
+
+#-------------------------------
+# S3 Remote State
+#-------------------------------
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-bucket-terraform-state"
+    key    = "vpc.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count      = "${length(var.public_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.public_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "public_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count      = "${length(var.private_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.private_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "private_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "database_subnet" {
+  count      = "${length(var.database_subnet_cidr_block)}"
+
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "${element(var.database_subnet_cidr_block, count.index)}"
+
+  availability_zone = "${element(var.availability_zones, count.index)}"
+
+  tags = {
+    Name = "database_subnet_${count.index}"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+# EIP and NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${element(aws_subnet.public_subnet.*.id, 1)}"
+  depends_on = ["aws_internet_gateway.igw"]
+}
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  count = "${length(aws_subnet.public_subnet.*.id)}"
+
+  subnet_id      = "${element(aws_subnet.public_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  count = "${length(aws_subnet.private_subnet.*.id)}"
+
+  subnet_id      = "${element(aws_subnet.private_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private_route_table.id}"
+}
+
+# Database Route Table
+resource "aws_route_table" "database_route_table" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.natgw.id}"
+  }
+}
+
+resource "aws_route_table_association" "database_rt_association" {
+  count = "${length(aws_subnet.database_subnet.*.id)}"
+
+  subnet_id      = "${element(aws_subnet.database_subnet.*.id, count.index)}"
+  route_table_id = "${aws_route_table.database_route_table.id}"
+}
+
+resource "aws_instance" "gemfire_server" {
+  ami           = "ami-02045ebddb047018b"
+  instance_type = "t2.micro"
+  key_name      = "ec2-key"
+  security_groups = ["${aws_security_group.allow_ssh.id}"]
+  subnet_id = "${element(aws_subnet.public_subnet.*.id, 1)}"
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh.id
+  ]
+  tags = {
+    Name = var.ec2_name
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow ssh"
+  description = "only ssh"
+  vpc_id = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "gemfire_server_2" {
+  ami           = "ami-02045ebddb047018b"
+  instance_type = "t2.micro"
+  key_name      = "ec2-key"
+  security_groups = ["${aws_security_group.allow_ssh_default.id}"]
+  tags = {
+    Name = "gemfire_server_2"
+  }
+}
+
+resource "aws_security_group" "allow_ssh_default" {
+  name        = "allow ssh"
+  description = "only ssh"
+  vpc_id = "vpc-0ccd16672c8189087"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
